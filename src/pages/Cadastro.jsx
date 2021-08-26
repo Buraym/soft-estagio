c65188c4b7310d0b71839d01f97781ee59eb10d4
Brayan <BrayanWilis@outlook.com>
2021-08-26 11:30:08 -0300

    feat: salvar na localStorage, form funcional e estilizado, pagina consulta desenvolvida front parcialmente

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BsFillCaretDownFill } from "react-icons/bs";
 import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import '../styles/Cadastro.css'
+import { Container } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import { v4 as uuidv4 } from 'uuid';
 
 function Cadastro() {
 
     const [isOpen, setIsOpen] = useState(false)
-    const [pattern, setPattern] = useState("[0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2}")
-    const [id, setId] = useState(0)
-    const [tipo, setTipo] = useState("CPF")
+    const [pattern, setPattern] = useState("[0-9]{2}?[0-9]{3}?[0-9]{3}?[0-9]{4}[-]?[0-9]{2}")
+    var id = uuidv4()
+    const [tipo, setTipo] = useState("Fisica")
     const [cpfCnpj, setCpfCnpj] = useState(0)
     const [nome, setNome] = useState("")
     const [nomeFantasia, setNomeFantasia] = useState("")
@@ -18,6 +25,8 @@ function Cadastro() {
     const [cidade, setCidade] = useState("")
     const [telefone, setTelefone] = useState(0)
     const [email, setEmail] = useState("")
+
+    let history = useHistory();
     
     function HandleChangeCpfCnpj(e) {setCpfCnpj(e.target.value)}
     function HandleChangeNome(e) {setNome(e.target.value)}
@@ -29,8 +38,49 @@ function Cadastro() {
     function HandleChangeTelefone(e) {setTelefone(e.target.value)}
     function HandleChangeEmail(e) {setEmail(e.target.value)}
 
-    function HandleSubmit(e){
-        e.preventDefault();
+    function HandleSubmit(){
+        
+        /* window.localStorage.setItem("usuarios", "")
+        var listaVazia = []
+        window.localStorage.setItem("usuarios", listaVazia)
+        console.log(window.localStorage.getItem("usuarios"))
+        var listaNova = [JSON.stringify(usuario)]
+        window.localStorage.setItem("usuarios", listaNova)
+        console.log(window.localStorage.getItem("usuarios"))
+        var listaAtual = window.localStorage.getItem("usuarios")
+        console.log((listaAtual)) */
+        
+        const listaUsuarios = window.localStorage.getItem("usuarios")
+
+        if (listaUsuarios === null ){
+            console.log("Caso da lista vazia")
+            var listaCadastro = JSON.stringify(usuario)
+            window.localStorage.setItem("usuarios", listaCadastro)
+            console.log(window.localStorage.getItem("usuarios"))
+            var listaNova = [window.localStorage.getItem("usuarios")]
+            console.log(JSON.parse(listaNova))
+            window.localStorage.setItem("usuarios", listaNova)
+            var listaAtual = window.localStorage.getItem("usuarios")
+
+            /* Para Obter a lista é so a usar a linha 62 e dar um parse nela 
+            var listaAtual = window.localStorage.getItem("usuarios")
+            console.log((JSON.parse(listaAtual)))*/
+
+
+        } else {
+            console.log("Caso da lista que não está cheia")
+            var listaCadastros = [window.localStorage.getItem("usuarios")]
+            console.log(listaCadastros)
+            listaCadastros.push(JSON.stringify(usuario))
+            console.log(listaCadastros)
+            window.localStorage.setItem("usuarios", listaCadastros)
+            console.log(window.localStorage.getItem("usuarios"))
+            /*window.localStorage.removeItem("usuarios")*/
+            
+            /*var listaAdicionar = JSON.stringify(listaUsuarios)
+            window.localStorage.setItem("usuarios", JSON.stringify(listaAdicionar))*/
+        }
+        /*history.push('/consulta')*/
     }
 
     const usuario = {
@@ -49,60 +99,79 @@ function Cadastro() {
 
     return(
         <div className="tela">
-            <form className="formulario" onSubmit={HandleSubmit}>
-                <div className="formulario-tela">
-                    <div className="formulario-bloco">
-                        <label>Tipo:</label>
-                        <BsFillCaretDownFill onClick={()=>(setIsOpen(!isOpen))}/>
-                        {
-                            isOpen ?
-                                <div> 
-                                    <Button variant="dark" size="sm" onClick={()=>(setTipo("CPF"),setPattern("[0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2}"))} active> CPF </Button>
-                                    <Button variant="dark" size="sm" onClick={()=>(setTipo("CNPJ"),setPattern("[0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2}"))} active> CNPJ </Button>
-                                </div>
-                            :
-                                null
-                        }
-                    </div>
-                    <div className="formulario-bloco">
-                        <label>CPF/CNPJ:</label>
-                        <input onChange={HandleChangeCpfCnpj} pattern={pattern} placeholder={ (tipo === "CPF") ? "XXX.XXX.XXX-XX" : "XX.XXX.XXX.XXXX.XX"}/>
-                    </div>
-                    <div className="formulario-bloco">
-                        <label>NOME:</label>
-                        <input onChange={HandleChangeNome}/>
-                    </div>
-                    <div className="formulario-bloco">
-                        <label>NOME FANTASIA:</label>
-                        <input onChange={HandleChangeNomeFantasia}/>
-                    </div>
-                    <div className="formulario-bloco">
-                        <label>CEP:</label>
-                        <input onChange={HandleChangeCep}/>
-                    </div>
-                    <div className="formulario-bloco">
-                        <label>ENDEREÇO:</label>
-                        <input onChange={HandleChangeEndereco}/>
-                    </div>
-                    <div className="formulario-bloco">
-                        <label>BAIRRO:</label>
-                        <input onChange={HandleChangeBairro}/>
-                    </div>
-                    <div className="formulario-bloco">
-                        <label>CIDADE:</label>
-                        <input onChange={HandleChangeCidade}/>
-                    </div>
-                    <div className="formulario-bloco">
-                        <label>TELEFONE:</label>
-                        <input type="tel" pattern="[0-9]{2}[0-9]{5}-[0-9]{4}" onChange={HandleChangeTelefone}></input>
-                    </div>
-                    <div className="formulario-bloco">
-                        <label>EMAIL:</label>
-                        <input onChange={HandleChangeEmail}/>
-                    </div>
-                </div>
-                <div><button>Salvar</button></div>
-            </form>
+            <label><strong>Tela de Cadastro</strong></label>
+            <Form>
+                <Container fluid="sm">
+                    <Row>
+                        <Col md={{ span: 4, offset: 4 }}>
+                            <div className="formulario-bloco">
+                                <label>Tipo:</label>
+                                <BsFillCaretDownFill onClick={()=>(setIsOpen(!isOpen))}/>
+                                {
+                                    isOpen ?
+                                        <Row md={{ span: 1, offset: 1 }}>
+                                            <Col md={{ span: 4, offset: 4 }}>
+                                                <Button variant="dark" size="sm" onClick={()=>(setTipo("Fisica"),setPattern("[0-9]{3}?[0-9]{3}?[0-9]{3}[-]?[0-9]{2}"))} active> CPF </Button>
+                                            </Col>
+                                            <Col md={{ span: 4, offset: 4 }}>
+                                                <Button variant="dark" size="sm" onClick={()=>(setTipo("Juridica"),setPattern("[0-9]{2}?[0-9]{3}?[0-9]{3}?[0-9]{4}[-]?[0-9]{2}"))} active> CNPJ </Button>
+                                            </Col>
+                                            
+                                        </Row>
+                                    :
+                                        null
+                                }
+                            </div>
+                            <Form.Label>{ (tipo === "CPF") ? "CPF: " : "CNPJ: "}</Form.Label>
+                            <Form.Control onChange={HandleChangeCpfCnpj}  pattern={pattern} placeholder={ (tipo === "Fisica") ? "XXXXXXXXXXX" : "XXXXXXXXXXXXXX"}/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={{ span: 4, offset: 4 }}>
+                            <Form.Label>NOME: </Form.Label>
+                            <Form.Control onChange={HandleChangeNome} required/>
+                        </Col>
+                        <Col md={{ span: 4, offset: 4 }}>
+                            <Form.Label>NOME FANTASIA: </Form.Label>
+                            <Form.Control onChange={HandleChangeNomeFantasia} required/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={{ span: 4, offset: 4 }}>
+                            <Form.Label>CEP: </Form.Label>
+                            <Form.Control onChange={HandleChangeCep} required/>
+                        </Col>
+                        <Col md={{ span: 4, offset: 4 }}>
+                            <Form.Label>ENDEREÇO: </Form.Label>
+                            <Form.Control onChange={HandleChangeEndereco} required/>
+                        </Col>
+                        <Col md={{ span: 4, offset: 4 }}>
+                            <Form.Label>BAIRRO: </Form.Label>
+                            <Form.Control onChange={HandleChangeBairro} required/>
+                        </Col>
+                        <Col md={{ span: 4, offset: 4 }}>
+                            <Form.Label>CIDADE: </Form.Label>
+                            <Form.Control onChange={HandleChangeCidade} required/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={{ span: 4, offset: 4 }}>
+                            <Form.Label>TELEFONE: </Form.Label>
+                            <Form.Control onChange={HandleChangeTelefone} required/>
+                        </Col>
+                        <Col md={{ span: 4, offset: 4 }}>
+                            <Form.Label>EMAIL: </Form.Label>
+                            <Form.Control onChange={HandleChangeEmail} required/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={{ span: 4, offset: 4 }}>
+                            <Button variant="dark" onClick={HandleSubmit}> SALVAR </Button>
+                        </Col>
+                    </Row>
+                </Container>
+            </Form>
+                
         </div>
     )
 }
--- src/pages/Consulta.jsx
+++ src/pages/Consulta.jsx
@@ -1,37 +1,35 @@
 import '../styles/Consulta.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from "react-bootstrap";
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+import { Link } from 'react-router-dom';
 
 function Consulta() {
 
+    const listaString = JSON.parse(window.localStorage.getItem("usuarios"))
+    
+
     return(
         <div className="tela">
-            <button>NOVO</button>
-
-            <div className="tabela">
-                <div className="tabela-bloco">
-                    <div className="tabela-caixa"></div>
-                    <div className="tabela-lista">
-                        <li className="lista"></li>
-                    </div>
-                </div>
-                <div className="tabela-bloco">
-                    <div className="tabela-caixa-lista"></div>
-                    <div className="tabela-lista">
-                        <li className="lista"></li>
-                    </div>
-                </div>
-                <div className="tabela-bloco">
-                    <div className="tabela-caixa-lista"></div>
-                    <div className="tabela-lista">
-                        <li className="lista"></li>
-                    </div>
-                </div>
-                <div className="tabela-bloco">
-                    <div className="tabela-caixa-lista"></div>
-                    <div className="tabela-lista">
-                        <li className="lista"></li>
-                    </div>
-                </div>
-            </div>
+            <Row><Link to="/cadastro"><Button variant="dark">NOVO</Button></Link></Row>
+            <Container>
+                <Col>
+                    <Row>
+                        <Col xs={1,2} class="row">Código</Col>
+                        <Col xs={3} class="row">Nome</Col>
+                        <Col xs={3} sclass="row">Endereço</Col>
+                        <Col xs={3} sclass="row">Email</Col>
+                    </Row>
+                    <Row>
+                        {listaString.forEach((el) => {return(<Col key={el}>{el.id}</Col>)})}
+                        {listaString.forEach((el) => {return(<Col key={el}>{el.nome}</Col>)})}
+                        {listaString.forEach((el) => {return(<Col key={el}>{el.endereco}</Col>)})}
+                        {listaString.forEach((el) => {return(<Col key={el}>{el.email}</Col>)})}
+                    </Row>
+                </Col>
+            </Container>
         </div>
     )
 }
